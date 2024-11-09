02bdcb9805e607ff6e9276bba18b012f9c2a0ee5
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2024-11-09 18:39:54 -0500

    update menu from small devices

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,21 @@
 import Icons from "./Icons";
 import { Link } from "react-scroll";
+import { Squash as Hamburger } from "hamburger-react";
+import { useState, useEffect } from "react";
 
 function Menu() {
   //Creamos esta variable para corregir el scroll que se modifica debido a la animacion del DOM
   let vh = window.innerHeight;
+  let vw = window.innerWidth;
   let srollCorrection = -0.2 * vh;
+
+  const [isOpen, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (vw > 850) {
+      setOpen(true);
+    }
+  }, []);
 
   return (
     <>
@@ -12,53 +23,68 @@ function Menu() {
         <section className="menuLeft">
           <Icons iconName="code"></Icons>
         </section>
-        <section className="menuRight">
-          <Link
-            style={{ cursor: "pointer" }}
-            to="skills"
-            smooth={true}
-            offset={srollCorrection}
-            duration={800}
-          >
-            Habilidades
-          </Link>
-          <Link
-            style={{ cursor: "pointer" }}
-            to="aboutMe"
-            smooth={true}
-            offset={srollCorrection}
-            duration={800}
-          >
-            Sobre mí
-          </Link>
-          <Link
-            style={{ cursor: "pointer" }}
-            to="portfolio"
-            smooth={true}
-            offset={srollCorrection}
-            duration={800}
-          >
-            Proyectos
-          </Link>
-          <Link
-            style={{ cursor: "pointer" }}
-            to="education"
-            smooth={true}
-            offset={srollCorrection}
-            duration={800}
-          >
-            Formación
-          </Link>
-          <Link
-            style={{ cursor: "pointer" }}
-            to="contact"
-            smooth={true}
-            offset={srollCorrection}
-            duration={800}
-          >
-            Contacto
-          </Link>
-        </section>
+        <span className="spanMenu"></span>
+        {vw <= 820 && (
+          <div className="hamburger">
+            <Hamburger
+              className="hamburger"
+              rounded
+              color="white"
+              size={vw < 500 ? 30 : 40}
+              toggled={isOpen}
+              toggle={setOpen}
+            />
+          </div>
+        )}
+        {isOpen && (
+          <section className="menuRight">
+            <Link
+              style={{ cursor: "pointer" }}
+              to="skills"
+              smooth={true}
+              offset={srollCorrection}
+              duration={800}
+            >
+              Habilidades
+            </Link>
+            <Link
+              style={{ cursor: "pointer" }}
+              to="aboutMe"
+              smooth={true}
+              offset={srollCorrection}
+              duration={800}
+            >
+              Sobre mí
+            </Link>
+            <Link
+              style={{ cursor: "pointer" }}
+              to="portfolio"
+              smooth={true}
+              offset={srollCorrection}
+              duration={800}
+            >
+              Proyectos
+            </Link>
+            <Link
+              style={{ cursor: "pointer" }}
+              to="education"
+              smooth={true}
+              offset={srollCorrection}
+              duration={800}
+            >
+              Formación
+            </Link>
+            <Link
+              style={{ cursor: "pointer" }}
+              to="contact"
+              smooth={true}
+              offset={srollCorrection}
+              duration={800}
+            >
+              Contacto
+            </Link>
+          </section>
+        )}
       </nav>
     </>
   );
